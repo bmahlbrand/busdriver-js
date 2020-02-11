72c1231f909cf841d79e978342447b082490eb0d
Ben Ahlbrand <bmahlbrand@gmail.com>
2020-02-11 13:51:22 -0500

    actual code

---
+++ EventBus.js
@@ -0,0 +1,86 @@
+// TODO:
+// priority scheduling
+// this + networking - linearizability...
+// browser + node compatibility
+// decorators
+// change unique id to some hashing algorithm
+
+class EventBus {
+    constructor() {
+        this.callbacks = {};
+        this.currentId = 0;
+    }
+
+    channels() {
+        return Object.keys(this.callbacks);
+    }
+
+    uniqueId() {
+        return this.currentId++ % Number.MAX_SAFE_INTEGER;
+    }
+
+    unsubscribeAll() {
+        this.callbacks = {};
+    }
+
+    unsubscribe(channel, id) {
+        delete this.callbacks[channel][id];
+        if (Object.keys(this.callbacks[channel]).length === 0) {
+            delete this.callbacks[channel];
+        }
+    }
+
+    subscribe(channel, callback, scope, ...args) {
+        const id = this.uniqueId();
+
+        if (!this.callbacks[channel])
+            this.callbacks[channel] = {};
+
+        this.callbacks[channel][id] = {callback, scope, args};
+
+        return { 
+            unsubscribe: () => {
+                return this.unsubscribe(channel, id);
+            }
+        };
+    }
+
+    // useful for one-time events,
+    // removes the callback after only one use
+    subscribeOneShot(channel, callback) {
+        const id = this.uniqueId();
+
+        if (!this.callbacks[channel])
+            this.callbacks[channel] = {};
+
+        this.callbacks[channel][id] = (args) => {
+            this.unsubscribe(channel, id);
+            return callback(args);
+        };
+
+        return id;
+
+    }
+
+    // return subscribers to a channel
+    subscribers(channel) {
+        return Object.keys(this.callbacks[channel]);
+    }
+
+    publish(channel, event) {
+        if (channel in this.callbacks && Object.keys(this.callbacks[channel]).length > 0) {
+
+            for (const key in this.callbacks[channel]) {                
+                const obj = this.callbacks[channel][key];
+                obj.callback(event, obj.scope);
+                // obj.apply(event, obj.scope, ...obj.args);
+                // console.log(obj);
+            }
+            
+        } else {
+            console.warn("message of type %s is not supported yet", channel);
+        }
+    }
+};
+
+module.exports = EventBus;
---
+++ tests/EventBus.test.js
@@ -0,0 +1,80 @@
+const EventBus = require("../EventBus.js");
+
+test('EventBus', () => {
+
+    const bus = new EventBus();
+    expect(bus).not.toBeNull();
+
+});
+
+test('EventBus:uniqueId', () => {
+
+    const bus = new EventBus();
+
+    expect(bus.uniqueId()).toEqual(0);
+    expect(bus.uniqueId()).toEqual(1);
+    expect(bus.uniqueId()).not.toEqual(1);
+
+});
+
+test('EventBus: subscribe / unsubscribe', () => {
+
+    const bus = new EventBus();
+
+    const platformToken1 = bus.subscribe("platform", (json) => {});
+
+    const tickToken = bus.subscribe("tick", (json) => {});
+
+    expect(bus.channels().sort()).toEqual(["platform", "tick"].sort());
+
+    const subscribers1 = bus.subscribers("tick");
+    expect(subscribers1).toEqual(["1"]);
+
+    tickToken.unsubscribe();
+
+    expect(bus.channels().sort()).toEqual(["platform"].sort());
+
+    const subscribers2 = bus.subscribers("platform");
+    expect(subscribers2).toEqual(["0"]);
+
+    const platformToken2 = bus.subscribe("platform", (json) => {});
+
+    const subscribers3 = bus.subscribers("platform");
+    expect(subscribers3).toEqual(["0", "2"]);
+
+    platformToken1.unsubscribe();
+
+    const subscribers4 = bus.subscribers("platform");
+    expect(subscribers4).toEqual(["2"]);
+
+});
+
+test('EventBus: scope', () => {
+
+    const bus = new EventBus();
+
+    class Class1 {
+        constructor() {
+            this.className = "Class1";
+            bus.subscribe("a", this.callback, this);
+        }
+
+        callback(event, scope) {
+            console.log(scope.className + " / event: " + event);
+        }
+    }
+    
+    class Class2 {
+        constructor() {
+            this.className = "Class2";
+        }
+
+        fire() {
+            bus.publish("a", this);
+        }
+    }
+    
+    let t1 = new Class1();
+    let t2 = new Class2();
+    t2.fire();
+});
